8ce486d6be7c29a15f9e8e656e6fe6009f668fcd
abdullahi <moh.abdullahi2003@gmail..com>
2024-04-03 22:10:16 +0100

    added projects done

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -1,5 +1,6 @@
 import Card from "./card"
 import htmldemo from "../assets/HTMLDemo.png";
+import wowtouche from '../assets/wowtouche.png'
 import burgerapp from "../assets/burgerapp.png";
 import greencandlestick from "../assets/greencandlestick.png";
 import breej from "../assets/breej.png";
@@ -10,44 +11,44 @@ const Portfolio = () => {
 
     const projects = [{
         heading: "wowtouche",
-        image: htmldemo,
+        image: wowtouche,
         paragraph: "Wowtouche is a hair braiding place that is built to enhance the business sales. And it has improved sales by 80%",
-        stacks: [{ stack: "wix" }],
-        links: [{ title: "GitHub", link: "html" }, { title: "Website", link: "css" }]
+        stacks: [{ stack: "wix" }, { stack: "wix editor" }],
+        links: [{ title: "GitHub: private", link: "#" }, { title: "Website", link: "https://www.wowtouche.com/" }]
     }, {
         heading: "Burger Builder",
         image: burgerapp,
-        paragraph: "hello",
-        stacks: [{ stack: "html" }, { stack: "css" }],
-        links: [{ title: "GitHub", link: "html" }, { title: "Website", link: "css" }]
+        paragraph: "i built this app when i was learning react from jonas schmedtmann course.",
+        stacks: [{ stack: "react" }, { stack: "firebase" }],
+        links: [{ title: "GitHub", link: "https://github.com/mabdul45/burger-app" }, { title: "Website", link: "https://burger-app-delta.vercel.app/" }]
     },
     {
-        heading: "wowtouche",
+        heading: "Green Candle Stick",
         image: greencandlestick,
-        paragraph: "hello",
-        stacks: [{ stack: "html" }, { stack: "css" }],
-        links: [{ title: "GitHub", link: "html" }, { title: "Website", link: "css" }]
+        paragraph: "Green candle stick is a web3 staking dapp. Users can connect their wallets, stake and claim tokens. its secured.",
+        stacks: [{ stack: "react" }, { stack: "web3.js" }],
+        links: [{ title: "GitHub", link: "https://github.com/mabdul45/green-candle-stick-demo" }, { title: "Website", link: "https://green-candle-stick-demo.vercel.app/" }]
     },
     {
-        heading: "wowtouche",
+        heading: "Tip Calculator",
         image: tipcalculator,
-        paragraph: "hello",
-        stacks: [{ stack: "html" }, { stack: "css" }],
-        links: [{ title: "GitHub", link: "html" }, { title: "Website", link: "css" }]
+        paragraph: "tip calcultor is a web app that is used to calculate individual tips based on input. it's built from frontend mentor.",
+        stacks: [{ stack: "react" }],
+        links: [{ title: "GitHub", link: "https://github.com/mabdul45/tip-calculator" }, { title: "Website", link: "https://tip-calculator-mabdul45.vercel.app/" }]
     },
     {
-        heading: "wowtouche",
+        heading: "Breej",
         image: breej,
-        paragraph: "hello",
-        stacks: [{ stack: "html" }, { stack: "css" }],
-        links: [{ title: "GitHub", link: "html" }, { title: "Website", link: "css" }]
+        paragraph: "breej is a platform for freelancers. it's not only for selling of services but also products. I helped built the buying and swapping features of this website.",
+        stacks: [{ stack: "Next.js" }, { stack: "TypeScript" }, { stack: "TailwindCSS" }],
+        links: [{ title: "GitHub: private repo", link: "#" }, { title: "Website", link: "https://breej.io/" }]
     },
     {
-        heading: "wowtouche",
+        heading: "Plut",
         image: plut,
-        paragraph: "hello",
-        stacks: [{ stack: "html" }, { stack: "css" }],
-        links: [{ title: "GitHub", link: "html" }, { title: "Website", link: "css" }]
+        paragraph: "plut is a platform for selling giftcards and coins. I helped integrate content management sysytem into this website whiich made it possible for the client to be able to change contents on the website easily.",
+        stacks: [{ stack: "React.js" }, { stack: "TypesScript" }, { stack: "Ionic Framework" }, { stack: "Firebase" }],
+        links: [{ title: "GitHub: private repo", link: "#" }, { title: "Website", link: "https://plut.ng/" }]
     },
     ]
 
@@ -66,12 +67,12 @@ const Portfolio = () => {
                             <p>{project.paragraph}</p>
                             <ul className="flex gap-2">
                                 {project.stacks.map((stack, i) => {
-                                    return <li className="bg-[#161616] text-[14px] rounded-lg py-1 px-2" key={i}>{stack.stack}</li>
+                                    return <li className="dark:bg-[#161616] dark:text-white bg-[#bebebe] text-[#030303] text-[14px] rounded-lg py-1 px-2" key={i}>{stack.stack}</li>
                                 })}
                             </ul>
                             <div className="flex justify-between items-center mt-auto">
                                 {project.links.map((link, i) => {
-                                    return <a href={link.link} key={i}>{link.title}</a>
+                                    return <a className="hover:underline" href={link.link} target="_blank" key={i}>{link.title}</a>
                                 })}
                             </div>
                         </Card>
